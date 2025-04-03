1fd7eda08d76a9048df413796fded540e2bb8921
Ahmad ameen <156881672+Ahmadamin1234@users.noreply.github.com>
2025-04-03 11:14:21 +0500

    simple login form not data base attached

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+    document.getElementById("send").onclick=function(){
+        var name=document.getElementById("name").value 
+        var password=document.getElementById("pass").value
+          if(name==""){
+            name="NULL"
+          }
+          if(password==""){
+            password="NULL"
+          }
+          }
+    }
